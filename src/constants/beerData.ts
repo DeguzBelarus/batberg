cf41573e46f6a74c2d874fe3dcdd991d7942ae86
DeguzBelarus <deguz@mail.ru>
2023-05-25 22:50:13 +0300

    refactor: add color for text in cards

--- src/constants/beerData.ts
+++ src/constants/beerData.ts
@@ -8,7 +8,8 @@ export const beerData: Array<IBeerData> = [
     alc: 6.5,
     amount: 1,
     density: 16.5,
-    description: 'Описание...',
+    description:
+      'Сварен с использованием бельгийского леденцового сахара. Во вкусе солодовая сладость и мягкий хмелевой профиль. Имеет пряные, фенольные тона характерные для бельгийских элей.',
   },
   {
     name: 'Weizenbock',
@@ -16,8 +17,9 @@ export const beerData: Array<IBeerData> = [
     subheading: 'BOCK',
     alc: 7.0,
     amount: 1,
-    density: 17,
-    description: 'Описание...',
+    density: 17.0,
+    description:
+      'Немецкий пшеничный эль. Во вкусе тона белого хлеба, зерна. А так-же лёгкие гвоздичные ноты. Горечь маленькая.',
   },
   {
     name: 'Венский лагер',
@@ -25,8 +27,9 @@ export const beerData: Array<IBeerData> = [
     subheading: 'LAGGER',
     alc: 5.5,
     amount: 1,
-    density: 14,
-    description: 'Описание...',
+    density: 14.0,
+    description:
+      'Это полутёмный сорт . Сварен с использованием благородного немецкого хмеля Селект. Профиль насыщенный. Слегка сладковатый.',
   },
   {
     name: 'Американский лагер',
@@ -34,7 +37,8 @@ export const beerData: Array<IBeerData> = [
     alc: 5,
     amount: 1,
     density: 12.5,
-    description: 'Описание...',
+    description:
+      'Классический немецкий ржаной лагер. Сварен с использованием ржаного ферментированного солода. Во вкусе корочка бородинского хлеба, умеренные ореховые ноты и немножко карамели.',
   },
   {
     name: 'Ржаной лагер',
@@ -42,7 +46,8 @@ export const beerData: Array<IBeerData> = [
     subheading: 'BEER',
     alc: 5.5,
     amount: 1,
-    density: 15,
-    description: 'Описание...',
+    density: 15.0,
+    description:
+      'Лёгкий, питкий лагер с хорошей горчинкой. Было сделано сухое охмеление американским хмелем Мозаик.',
   },
 ];
--- src/pages/MainPage/components/BeerCard/BeerCard.tsx
+++ src/pages/MainPage/components/BeerCard/BeerCard.tsx
@@ -13,20 +13,23 @@ export const BeerCard: FC<Props> = ({
   id,
   data: { alc, amount, density, description, heading, name, subheading },
 }) => {
+  const headingColors = ['b42127', 'cc9052', '51ccba', '525fd1', 'a853cc'];
   return (
     <div className="beer-card-wrapper">
       <div className="upper-container">
         <div className="poster-container">
           <img className="poster" src={batBergLogo} alt="barrel with beer and a bat label" />
-          <p className="beer-data-paragraph">{`${amount}L ${density} ${alc}%/Vol`}</p>
+          <p className="beer-data-paragraph">{`${amount}L ${density.toFixed(1)} ${alc}%/Vol`}</p>
         </div>
         <div className="name-container">
-          <h2>{name}</h2>
-          <p>{description || ''}</p>
+          <h1>{name}</h1>
+          <p>{description}</p>
         </div>
       </div>
       <div className="info-container">
-        <p className="heading-paragraph">{heading}</p>
+        <p className="heading-paragraph" style={{ color: `#${headingColors[id]}` }}>
+          {heading}
+        </p>
         <p className="subheading-paragraph">{subheading || ''}</p>
         <p className="title-paragraph">Brewed by Batberg</p>
       </div>
